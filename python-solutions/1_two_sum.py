06451cd734c26cea6d78eea6207ab5c72b68f6a8
shirvani <shirvash96@gmail.com>
2022-10-28 20:26:00 +0400

    add solution

---
+++ python-solutions/1_two_sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        diff = {}
+        for i, num in enumerate(nums):
+            if num in diff:
+                return [diff[num], i]
+            diff[target-num] = i
