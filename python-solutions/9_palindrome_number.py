373bd490790c2d1557fc4e126906e28f5a3146ae
shirvani <shirvash96@gmail.com>
2022-10-28 20:25:53 +0400

    change solution

--- python-solutions/9_palindrome_number.py
+++ python-solutions/9_palindrome_number.py
@@ -1,15 +1,24 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        x = str(x)
-        length = len(x)
-        for i in range(int(length / 2)):
-            if x[i] == x[length - i - 1]:
-                pass
-            else:
-                return False
-        return True
+        return str(x) == str(x)[::-1]
 
-    def isPalindrome(self, x: int) -> bool:
-        return x == x[::-1]
+        # old solution
+        # if x < 0:
+        #     return False
+        # x = str(x)
+        # length = len(x)
+        # for i in range(int(length / 2)):
+        #     if x[i] == x[length - i - 1]:
+        #         pass
+        #     else:
+        #         return False
+        # return True
+
+solution = Solution()
+result = solution.isPalindrome(1234321)
+print(f'result: {result}')
+print('expected: True')
+
+result = solution.isPalindrome(1213421)
+print(f'result: {result}')
+print('expected: False')
